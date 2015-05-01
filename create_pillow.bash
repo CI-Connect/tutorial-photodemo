667d352d9587a1ca190cb968ae91afee8b896500
bala <dmbala@gmail.com>
2015-05-01 16:15:59 -0500

    create_pillow.bash edit

---
+++ create_pillow.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create the virtualenv
+virtualenv pillow
+
+# Find and copy into place the virtualenv software
+cp $(python -c 'import virtualenv; print virtualenv.__file__' | sed -e 's/pyc/py/') pillow/bin/
+cp $(which virtualenv) pillow/bin/
+
+# "activate" the virtualenv
+source pillow/bin/activate
+
+# Install PIL. The STATIC_DEPS variable uses static libraries for any compiled dependencies
+env STATIC_DEPS=true pip install Pillow
+
+# Now "deactivate" the virtualenv
+deactivate
+
+tar cf pillow.tar pillow
+
